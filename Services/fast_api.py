9a2d365e9511b048e2c5995652b0ca7201604c8d
Zachary Koo <koo_z@bellsouth.net>
2020-12-18 03:54:06 -0500

    testing fileresponse

--- Services/fast_api.py
+++ Services/fast_api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
@@ -8,12 +10,15 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
+"""
 @app.get("/html/{num}", response_class=HTMLResponse)
 async def getHTML(num):
     f = open("Data/w" + str(num) + ".html", encoding="utf8")
     line = f.read()
     return HTMLResponse(content=str(line), status_code=200)
+"""
 
-
+@app.get("/BDD_background")
+async def BDD_background():
+    return FileResponse("Data/BDD_images/Yukina1.jpg")
 
