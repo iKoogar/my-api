04fe1616f5d041168fa74291c6c3bdabc5164781
iKoogar <koo_z@bellsouth.net>
2023-06-21 17:48:42 -0400

    commit for bd branch

--- Services/fast_api.py
+++ Services/fast_api.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import os, random
 
 # from fastapi.responses import HTMLResponse
 
-
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello bro"}
+    return {"message": "Hello World"}
 
 @app.get("/BDD_background.png")
 async def BDD_background():
@@ -22,10 +29,10 @@ async def BDD_background():
     random_file_name = random.choice(os.listdir("Data/BDD_images"))
     return FileResponse("Data/BDD_images/" + random_file_name)
 
-@app.get("/aoba")
+@app.get("/text")
 async def BDD_background():
-    return FileResponse("Data/BDD_images/29ukLNL.png")
+    return FileResponse("Data/scratch.txt")
 
-@app.get("/aoba.png")
+@app.get("/pdf")
 async def BDD_background():
-    return FileResponse("Data/BDD_images/29ukLNL.png")
+    return FileResponse("Data/Sorting-Part_2.pdf")
